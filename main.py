53a4bc5bd75a24f4da4b97a10b60f91bc5a35ef3
jye6013 <jye6013@bths.edu>
2021-09-26 16:34:12 -0400

    Fixing back end

--- main.py
+++ main.py
@@ -22,17 +22,32 @@ async def render(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
     
 @app.post('/shortener')
-def read_url(long_url: str = Form(...)):
-    print(long_url)
-    # return url
-    return {'long_url': long_url, 'encrypted': getShortUrl(long_url), 
-    'decrypted': fernet.decrypt(urlMappings[long_url]).decode()}
+def read_url(url: str = Form(...)):
+
+    # short url    
+    if (shortUrlExists(url)):
+        print("short url exists")
+    else:
+        return {'long_url': url, 'encrypted': getShortUrl(url),
+         'mappings': urlMappings}
+
+
+
+def getLongUrl(short_url):
+    for key, value in urlMappings.iteritems():
+        if value == short_url:
+            return key
+
+def shortUrlExists(short_url):
+    if short_url in urlMappings.values():
+        return True
+    else:
+        return False
 
 def getShortUrl(long_url):
     if long_url not in urlMappings:
-        short_url = "asianpower.com/" + str(convertLongtoShortUrl(long_url))[-7:]
-        urlMappings[long_url] = convertLongtoShortUrl(long_url)
-    return short_url   
+        urlMappings[long_url] = "asianpower.com/" + str(convertLongtoShortUrl(long_url))[-7:]
+    return urlMappings[long_url]   
 
 def convertLongtoShortUrl(long_url):
     return fernet.encrypt(long_url.encode())
